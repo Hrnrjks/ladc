ff02be3c6b7c500da4127b999a45b0f8b84a1298
narkesau <narkesau@wwv003208.eu.tieto.com>
2017-07-04 17:31:43 +0300

    reformated JS files

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
 var express = require('express');
-var app 	= express();
-var http 	= require('http').Server(app);
-var io 		= require('socket.io')(http);
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 /*
-app.use(express.static(__dirname + '/public'));
-app.use('/Styles', express.static(__dirname + '/public/Styles'));
-app.use('/Scripts', express.static(__dirname + '/public/Scripts'));
-*/
+ app.use(express.static(__dirname + '/public'));
+ app.use('/Styles', express.static(__dirname + '/public/Styles'));
+ app.use('/Scripts', express.static(__dirname + '/public/Scripts'));
+ */
 
 app.get('/', function (req, res)
 {
-	res.send(express.static('public/dhdh.html'));
+    res.send(express.static('public/dhdh.html'));
 });
 
 var users = [];
 var userConn = 'A user has connected';
 var userDisc = 'A user has disconnected';
-io.on('connection', function(socket)
+io.on('connection', function (socket)
 {
- 	console.log('a user has connected to ladc');
-	io.emit('user connected', "SERVER: " + userConn);
-	/*socket.on('user connected', function()
-	{
-		io.emit('user connected');
-	});*/
+    console.log('a user has connected to ladc');
+    io.emit('user connected', "SERVER: " + userConn);
+    /*socket.on('user connected', function()
+     {
+     io.emit('user connected');
+     });*/
 
-	socket.on('disconnect', function()
-	{
-		//console.log('a user has disconnected from ladc');
-		io.emit('user disconnected', "SERVER: " + userDisc);
-	});
+    socket.on('disconnect', function ()
+    {
+        //console.log('a user has disconnected from ladc');
+        io.emit('user disconnected', "SERVER: " + userDisc);
+    });
 
-	socket.on('chat message', function(msg)
-	{
-		//console.log('message: ' + msg);
-		io.emit('chat message', "Anon: " + msg);
-	});
-	
- 	socket.on('set username', function(data)
-	{
-		if (users.indexOf(data) > -1)
-		{
-			users.push(data);
-			socket.emit('user set', {username: data});
-		}
+    socket.on('chat message', function (msg)
+    {
+        //console.log('message: ' + msg);
+        io.emit('chat message', "Anon: " + msg);
+    });
 
-		else
-		{
-			socket.emit('user exists', data + ' username is taken.');
-		}
-	});
+    socket.on('set username', function (data)
+    {
+        if (users.indexOf(data) > -1)
+        {
+            users.push(data);
+            socket.emit('user set', {username: data});
+        } else
+        {
+            socket.emit('user exists', data + ' username is taken.');
+        }
+    });
 });
 
 http.listen(3000, function ()
 {
-	console.log('ladc up and running on port 3000');
+    console.log('ladc up and running on port 3000');
 });
