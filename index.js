426cac66af408a01096c45f67cf987b8459b869d
Hrnrjks <janulisvytas@gmail.com>
2017-08-04 17:07:43 +0300

    First working version

--- index.js
+++ index.js
@@ -13,47 +13,101 @@ app.get('/', function (req, res)
 {
     res.sendFile('private/index.html', {root: __dirname});
 });
+
+/*
 app.get('/login', function (req, res)
 {
     res.sendFile('private/login.html', {root: __dirname});
 });
+ */
 
 app.use(express.static('public'));
 
-var users = [];
+var users = {};
 var userConn = 'A user has connected';
 var userDisc = 'A user has disconnected';
+var connectCount = 0; // for unique username
 io.on('connection', function (socket)
 {
+	connectCount += 1;
+	var user = {
+		nick : null,
+		socket : socket,
+		ip : socket.handshake.address
+	};
+	
+	users[socket.id] = user;
+	
     console.log('a user has connected to ladc');
-    io.emit('user connected', "SERVER: " + userConn);
-    /*socket.on('user connected', function()
-     {
-     io.emit('user connected');
-     });*/
-
-    socket.on('disconnect', function ()
+    // io.emit('user connected', "SERVER: " + userConn);
+	
+    /*socket.on('user connected', function(nick)
     {
-        //console.log('a user has disconnected from ladc');
+		io.emit("'" + users[socket.id].nick + "' has emerged from nowhere.");
+    });*/
+
+	/*socket.on('join', function(name)
+	{
+		users[socket.id] = name;
+		socket.emit('update', "You're connected.");
+	 	socket.emit('update', name + " has emerged from nowhere.");
+		socket.emit('update-people', users);	
+	});
+
+ 	socket.on('send', function(msg)
+	{
+		socket.emit('chat message', users[socket.id], msg);
+	}); */
+
+	socket.on('disconnect', function()
+	{
+		/*socket.emit('update', users[socket.id] + " has mysteriously faded in plain sight.");
+		delete users[socket.id];
+		socket.emit('update-people', users);*/
+		
+		if (users[socket.id].nick != null)
+		{
+			io.emit('user disconnected', "'" + users[socket.id].nick + "@" + users[socket.id].ip + "' has mysteriously faded in plain sight.");
+		}
+		
+		delete users[socket.id];
+		//socket.emit('update-people', users);
+	});
+
+	/*socket.on('disconnect', function ()
+    {
+        console.log('a user has disconnected from ladc');
         io.emit('user disconnected', "SERVER: " + userDisc);
+    });*/
+
+	socket.on('chat message', function (msg)
+    {
+		var user = users[socket.id];
+        io.emit('chat message', user.nick + "@" + user.ip + ": " + msg); // make a foreach in users instead of this
     });
 
-    socket.on('chat message', function (msg)
+    socket.on('set nick', function (nick)
     {
-        //console.log('message: ' + msg);
-        io.emit('chat message', "Anonas: " + msg);
-    });
-
-    socket.on('set username', function (data)
-    {
-        if (users.indexOf(data) > -1)
+		for (var key in users)
         {
-            users.push(data);
-            socket.emit('user set', {username: data});
-        } else
-        {
-            socket.emit('user exists', data + ' username is taken.');
+            if (users[key].nick == nick)
+			{
+				socket.emit('nick exists', "'" + nick + "' nickname already exists.");
+				return false;
+			}
         }
+		
+		users[socket.id].nick = nick;
+		socket.emit('nick set', "'" + nick + "' nickname set successfully.");
+		
+		for (var key in users)
+		{
+			if (users[key].nick != nick)
+			{
+				users[key].socket.emit('user connected', "'" + users[socket.id].nick + "@" + users[socket.id].ip + "' has emerged from nowhere.");
+			}
+		}
+		// io.emit('user connected', "'" + users[socket.id].nick + "' has emerged from nowhere.");
     });
 });
 
--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -5,24 +5,40 @@ function scrollToBot()
 
 var connected = false; // make the first 'user connected' message triggered by current user not appear for them
 // the above solution doesn't work if the server was restarted while the website was still open on a browser
+var nickSet = false; // on first message the nickname is set to the message content and then you can message normally
+
 $(function()
 {
 	var socket = io();
 
-	$('form').submit(function()
+	
+	
+	$('#msg-form').submit(function()
 	{
-		socket.emit('chat message', $('#msg').val());
-		$('#msg').val('');
+		if (!nickSet)
+		{
+			socket.emit('set nick', $('#msg').val());
+			$('#msg').val('');
+		}
+		
+		else
+		{
+			socket.emit('chat message', $('#msg').val());
+			$('#msg').val('');
+		}
+		
 		return false;
 	});
+	
 	socket.on('chat message', function(msg)
 	{
 		$('#messages').append($('<li>').text(msg));
 		scrollToBot();
 	});
+	
 	socket.on('user connected', function(userConn)
 	{
-		if (connected)
+		/*if (connected)
 		{
  			$('#messages').append($('<li>').text(userConn));
  			scrollToBot();
@@ -31,12 +47,28 @@ $(function()
 		else
 		{
 			connected = true;
-		}
+		}*/
+		
+		$('#messages').append($('<li>').text(userConn));
+ 		scrollToBot();
 	});
+	
 	socket.on('user disconnected', function(userDisc)
 	{
 		$('#messages').append($('<li>').text(userDisc));
 		scrollToBot();
+	});
+	
+	socket.on('nick set', function(result)
+	{
+		socket.emit('user connected', "void");
+		$('#messages').append($('<li>').text(result));
+		nickSet = true;
+	});
+	
+	socket.on('nick exists', function(result)
+	{
+		$('#messages').append($('<li>').text(result));
 	});
 });
 
