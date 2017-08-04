4ead80b036eebe065331e7820052ff8a3a85f45e
Hrnrjks <janulisvytas@gmail.com>
2017-08-04 17:20:49 +0300

    Code cleanup

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-/*
- app.use(express.static(__dirname + '/public'));
- app.use('/Styles', express.static(__dirname + '/public/Styles'));
- app.use('/Scripts', express.static(__dirname + '/public/Scripts'));
- */
-
 app.get('/', function (req, res)
 {
     res.sendFile('private/index.html', {root: __dirname});
 });
-
-/*
-app.get('/login', function (req, res)
-{
-    res.sendFile('private/login.html', {root: __dirname});
-});
- */
 
 app.use(express.static('public'));
 
@@ -39,46 +26,16 @@ io.on('connection', function (socket)
 	users[socket.id] = user;
 	
     console.log('a user has connected to ladc');
-    // io.emit('user connected', "SERVER: " + userConn);
-	
-    /*socket.on('user connected', function(nick)
-    {
-		io.emit("'" + users[socket.id].nick + "' has emerged from nowhere.");
-    });*/
-
-	/*socket.on('join', function(name)
-	{
-		users[socket.id] = name;
-		socket.emit('update', "You're connected.");
-	 	socket.emit('update', name + " has emerged from nowhere.");
-		socket.emit('update-people', users);	
-	});
-
- 	socket.on('send', function(msg)
-	{
-		socket.emit('chat message', users[socket.id], msg);
-	}); */
 
 	socket.on('disconnect', function()
-	{
-		/*socket.emit('update', users[socket.id] + " has mysteriously faded in plain sight.");
-		delete users[socket.id];
-		socket.emit('update-people', users);*/
-		
-		if (users[socket.id].nick != null)
+	{	
+		if (user.nick != null)
 		{
-			io.emit('user disconnected', "'" + users[socket.id].nick + "@" + users[socket.id].ip + "' has mysteriously faded in plain sight.");
+			io.emit('user disconnected', "'" + user.nick + "@" + user.ip + "' has mysteriously faded in plain sight.");
 		}
 		
 		delete users[socket.id];
-		//socket.emit('update-people', users);
 	});
-
-	/*socket.on('disconnect', function ()
-    {
-        console.log('a user has disconnected from ladc');
-        io.emit('user disconnected', "SERVER: " + userDisc);
-    });*/
 
 	socket.on('chat message', function (msg)
     {
@@ -97,17 +54,16 @@ io.on('connection', function (socket)
 			}
         }
 		
-		users[socket.id].nick = nick;
+		user.nick = nick;
 		socket.emit('nick set', "'" + nick + "' nickname set successfully.");
 		
 		for (var key in users)
 		{
 			if (users[key].nick != nick)
 			{
-				users[key].socket.emit('user connected', "'" + users[socket.id].nick + "@" + users[socket.id].ip + "' has emerged from nowhere.");
+				users[key].socket.emit('user connected', "'" + user.nick + "@" + user.ip + "' has emerged from nowhere.");
 			}
 		}
-		// io.emit('user connected', "'" + users[socket.id].nick + "' has emerged from nowhere.");
     });
 });
 
